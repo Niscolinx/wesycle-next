89576708420063563209e9c1c88a806768b81b1a
Igboanugwo Collins <munisco12@gmail.com>
2020-12-02 15:14:22 +0100

    changed the folder structure

--- pages/index.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-    <h1>Welcome to Nextjs mastery</h1>
-}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+const App = () => (
+    <div>
+        Welcome to NextJs Mastery
+    </div>
+)
+
+export default App
