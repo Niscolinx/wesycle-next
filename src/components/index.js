1b95930fbd66a75e83f448f1010491d84908d6c3
Igboanugwo Collins <munisco12@gmail.com>
2020-12-02 16:04:42 +0100

    Added sass

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-const App = () => {
-    <div>
-        <h1>Welcome to the main app, in a short while, it will be changed!!</h1>
-    </div>
-}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div>
+            <h1 className='main'>
+                Welcome to the main app, in a short while, it will be changed!!
+            </h1>
+        </div>
+    )
+}
+
+export default App
