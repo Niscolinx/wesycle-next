95acf818db474d6b97f31874791ffb6340d40b03
Igboanugwo Collins <munisco12@gmail.com>
2020-12-02 14:54:33 +0100

    deleted the graphql files

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,3 @@
-import App from '../components/App'
-import InfoBox from '../components/InfoBox'
-import Header from '../components/Header'
-import Submit from '../components/Submit'
-import PostList, {
-    ALL_POSTS_QUERY,
-    allPostsQueryVars,
-} from '../components/PostList'
-import { initializeApollo, addApolloState } from '../lib/apolloClient'
-
-const IndexPage = () => (
-    <App>
-        <Header />
-        <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
-        <Submit />
-        <PostList />
-    </App>
-)
-
-export async function getStaticProps() {
-    const apolloClient = initializeApollo()
-
-    await apolloClient.query({
-        query: ALL_POSTS_QUERY,
-        variables: allPostsQueryVars,
-    })
-
-    return addApolloState(apolloClient, {
-        props: {},
-        revalidate: 1,
-    })
+export default () => {
+    <h1>Welcome to Nextjs mastery</h1>
 }
-
-export default IndexPage
