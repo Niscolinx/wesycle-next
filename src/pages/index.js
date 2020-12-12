7db33d16d06d26c44982094d3a8e9c0b84e07bef
Igboanugwo Collins <munisco12@gmail.com>
2020-12-12 06:53:25 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <div>
             <h1 className='main'>
-                Welcome to the main app, in a short while, it will be changed!!!
+                Welcome to the main app, in a short while, it will be changed!!
             </h1>
         </div>
     )
