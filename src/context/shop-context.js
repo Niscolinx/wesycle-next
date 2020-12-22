600c151f47ed9d833f28e68bcf8bf7a624b0b72e
Igboanugwo Collins <munisco12@gmail.com>
2020-12-22 12:47:17 +0100

    building the functions in react context api

--- src/context/shop-context.js
+++ src/context/shop-context.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default React.createContext({
     products: [
-        {id: 1, name: 'Gaming Mouse', price: 56.21},
-        {id: 2, name: 'Keyboard', price: 120.05},
-        {id: 3, name: 'Gaming Monitor', price: 1200.00},
-        {id: 4, name: 'Laptop', price: 2320.99},
+        {id: 1, title: 'Gaming Mouse', price: 56.21},
+        {id: 2, title: 'Keyboard', price: 120.05},
+        {id: 3, title: 'Gaming Monitor', price: 1200.00},
+        {id: 4, title: 'Laptop', price: 2320.99},
     ],
-    cart: []
+    cart: [],
+    addProductToCart: (productId) => {},
+    removeProductFromCart: (productId) => {},
 })
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,41 @@
+import React, {useState} from 'react'
 import ShopContext from '../context/shop-context'
 
 const App = () => {
+
+    const [state, setState] = useState({
+        products: [
+            { id: 1, title: 'Gaming Mouse', price: 56.21 },
+            { id: 2, title: 'Keyboard', price: 120.05 },
+            { id: 3, title: 'Gaming Monitor', price: 1200.0 },
+            { id: 4, title: 'Laptop', price: 2320.99 },
+        ],
+        cart: [],
+    })
+
+
+    const addProductToCart = productId => {
+        console.log('Hello world')
+    }
+
+    const removeProductFromCart = productId => {
+        console.log("Just added the product to cart!!")
+    }
+
     return (
-        <ShopContext>
+        <ShopContext.Provider value={{
+            products: state.products,
+            cart: state.cart,
+            addProductToCart: addProductToCart,
+            removeProductFromCart: removeProductFromCart
+        }}>
             <div>
                 <h1 className='main'>
                     Welcome to the main app, in a short while, it will be
                     changed!
                 </h1>
             </div>
-        </ShopContext>
+        </ShopContext.Provider>
     )
 }
 
