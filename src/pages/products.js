ec354afa02847586fb31180f522d4a352435b4a4
Igboanugwo Collins <munisco12@gmail.com>
2021-01-20 10:17:34 +0100

    Added the investor calculator

--- src/pages/products.js
+++ src/pages/products.js
@@ -4,9 +4,46 @@ import ShopContext from '../context/shop-context'
 
 const Products = () => {
     const context = useContext(ShopContext)
+
+    const inputInvestHandler = (e) => {
+        setInvest(e.target.value)
+    }
+
+    const investHandler = (e) => {
+        e.preventDefault()
+
+        const percent = 0.5
+        let profitMargin = invest * percent
+
+        let monthlyTotal = Math.floor(Number(profitMargin)) + Math.floor(Number(invest))
+
+        let monthlyReturn = []
+        let yearlyReturn = 0
+        for (let i = 0; i < 12; i++) {
+            yearlyReturn += monthlyTotal
+            monthlyReturn.push(monthlyTotal)
+
+            const gainMargin = monthlyTotal * percent
+            monthlyTotal = Math.floor(Number(monthlyTotal)) + Math.floor(Number(gainMargin))
+
+            console.log('final monthly total', monthlyTotal)
+        }
+
+        // const year =12
+
+        // const total = year.map((item, i) => {
+        //     return item
+        // })
+
+        setShowMonthly(monthlyReturn)
+
+        setShowTotal(yearlyReturn)
+
+        console.log('monthly', showMonthly)
+        console.log('yearly', yearlyReturn)
+    }
     return (
         <>
-
             <h2>
                 <LinkProps href='/'>Go back to home</LinkProps>
             </h2>
@@ -30,66 +67,63 @@ const Products = () => {
                     </button>
                 </div>
             ))}
-             <div
-                                    style={{
-                                        display: 'grid',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        margin: '0 25rem',
-                                    }}
-                                >
-                                    {showMonthly.map((item, i) => {
-                                        return (
-                                            <li
-                                                key={item}
-                                                style={{ fontSize: '1.5rem' }}
-                                            >
-                                                After month {i + 1} ={' '}
-                                                <span
-                                                    style={{
-                                                        fontSize: '2.5rem',
-                                                        fontWeight: 'bold',
-                                                    }}
-                                                >
-                                                    N{item.toLocaleString()}
-                                                </span>
-                                            </li>
-                                        )
-                                    })}
-                                    <h1
-                                        style={{
-                                            textAlign: 'center',
-                                            fontWeight: 'bold',
-                                            fontSize: '3rem',
-                                            margin: '3rem 0',
-                                        }}
-                                    >
-                                        N{showTotal.toLocaleString()}
-                                    </h1>
-                                    <form onSubmit={investHandler}>
-                                        <input
-                                            style={{
-                                                background: 'white',
-                                                height: '5rem',
-                                                fontSize: '3rem',
-                                                padding: '1rem',
-                                            }}
-                                            type='number'
-                                            onChange={inputInvestHandler}
-                                            value={invest}
-                                        />
-                                        <button
-                                            style={{
-                                                fontSize: '2rem',
-                                                textAlign: 'center',
-                                                padding: '1rem',
-                                                cursor: 'pointer',
-                                            }}
-                                        >
-                                            Submit
-                                        </button>
-                                    </form> 
-                                </div>
+            <div
+                style={{
+                    display: 'grid',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    margin: '0 25rem',
+                }}
+            >
+                {showMonthly.map((item, i) => {
+                    return (
+                        <li key={item} style={{ fontSize: '1.5rem' }}>
+                            After month {i + 1} ={' '}
+                            <span
+                                style={{
+                                    fontSize: '2.5rem',
+                                    fontWeight: 'bold',
+                                }}
+                            >
+                                N{item.toLocaleString()}
+                            </span>
+                        </li>
+                    )
+                })}
+                <h1
+                    style={{
+                        textAlign: 'center',
+                        fontWeight: 'bold',
+                        fontSize: '3rem',
+                        margin: '3rem 0',
+                    }}
+                >
+                    N{showTotal.toLocaleString()}
+                </h1>
+                <form onSubmit={investHandler}>
+                    <input
+                        style={{
+                            background: 'white',
+                            height: '5rem',
+                            fontSize: '3rem',
+                            padding: '1rem',
+                        }}
+                        type='number'
+                        onChange={inputInvestHandler}
+                        value={invest}
+                    />
+                    <button
+                        style={{
+                            fontSize: '2rem',
+                            textAlign: 'center',
+                            padding: '1rem',
+                            cursor: 'pointer',
+                        }}
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </>
     )
 }
